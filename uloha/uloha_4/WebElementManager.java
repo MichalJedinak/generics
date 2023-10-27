64c1118d06b07543d685a8a4cc86b0a088c32b1c
MichalJedinak <109184193+MichalJedinak@users.noreply.github.com>
2023-10-27 14:44:50 +0200

    uloha 4

---
+++ uloha/uloha_4/WebElementManager.java
@@ -0,0 +1,12 @@
+package main.java.com.skilmea.java.generics.uloha.uloha_4;
+
+import com.skilmea.java.generics.uloha.uloha_3.objects.Kniha;
+
+public interface WebElementManager {
+      void addButton(String text,String action);
+      void addForm(String name);
+      void addItem(String name);
+      Integer addBook(Kniha kniha);
+      List<Kniha> getAllBooks();
+      Kniha getBook(String title);//  or  int id  lepšie  pre výber s SQL databazy
+}
